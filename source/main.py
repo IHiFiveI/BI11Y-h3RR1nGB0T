3cccd40730b540df00c53a2260bc353881ffd555
IHiFiveI <subject6gfrt@gmail.com>
2020-06-05 19:58:56 +0700

    goose added

--- source/main.py
+++ source/main.py
@@ -6,10 +6,38 @@ token = f.read(59)
 f.close()
 prefix = ","
 
-client = commands.Bot(command_prefix = prefix)
+# client = commands.Bot(command_prefix = prefix)
+client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("Bot initialized")
+    print("Initialized bot: {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith(prefix):
+        return
+
+    if message.author == client.user:
+        return
+
+    goose_expectation = {'ЗАПУСКАЕМ\n░','g'}
+
+    for taken_goose in goose_expectation:
+        if message.content.startswith(taken_goose):
+            await message.channel.send('ЗАПУСКАЕМ\n'
+                                       '░ГУСЯ░▄▀▀▀▄░РАБОТЯГИ░░\n'
+                                       '▄███▀░◐░░░▌░░░░░░░\n'
+                                       '░░░░▌░░░░░▐░░░░░░░\n'
+                                       '░░░░▐░░░░░▐░░░░░░░\n'
+                                       '░░░░▌░░░░░▐▄▄░░░░░\n'
+                                       '░░░░▌░░░░▄▀▒▒▀▀▀▀▄\n'
+                                       '░░░▐░░░░▐▒▒▒▒▒▒▒▒▀▀▄\n'
+                                       '░░░▐░░░░▐▄▒▒▒▒▒▒▒▒▒▒▀▄\n'
+                                       '░░░▐░░░░▀▄▒▒▒▒▒▒▒▒▒▒▒▄\n'
+                                       '░░░░▀▄▄▄▄█▄▄▄▄▄▄▄▄▄▄▄▀▄\n'
+                                       '░░░░░░░░░░░▌▌░▌▌░░░░░\n'
+                                       '░░░░░░░░░░░▌▌░▌▌░░░░░\n'
+                                       '░░░░░░░░░▄▄▌▌▄▌▌░░░░░\n')
 
 client.run(token)
