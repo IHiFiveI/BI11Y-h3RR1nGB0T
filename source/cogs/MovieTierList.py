13905dac86e091e6bfe2d8a2a573f67087002d3d
IHiFiveI <57842399+IHiFiveI@users.noreply.github.com>
2020-06-12 18:30:47 +0700

    added function that shows list of films

--- source/cogs/MovieTierList.py
+++ source/cogs/MovieTierList.py
@@ -6,6 +6,15 @@ class MovieTierList(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+        with open('./json/film_list.json', 'r') as f:
+            self.films_loaded = json.loads(f.read())
 
     @commands.command()
-    async def mshow(self, ctx):
+    async def mlist(self, ctx):
+        self.list = 'Список просмотренных фильмов:\n'
+        for film in self.films_loaded:
+            self.list += film['Name'] + "\n"
+        await ctx.send(self.list)
+
+def setup(client):
+    client.add_cog(MovieTierList(client))
