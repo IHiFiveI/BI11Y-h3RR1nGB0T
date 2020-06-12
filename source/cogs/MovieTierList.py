af4bb0f27f1115ec1cc11494ec5f1ca3e87905a5
IHiFiveI <57842399+IHiFiveI@users.noreply.github.com>
2020-06-12 19:17:46 +0700

    attempt to write files

--- source/cogs/MovieTierList.py
+++ source/cogs/MovieTierList.py
@@ -6,6 +6,9 @@ class MovieTierList(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+        self.films_to_write = None
+        # with open('./json/film_list.json', 'w', encoding="utf-8") as f:
+        #     f.write(json.dumps(self.films_to_write, sort_keys=False, indent=2))
         with open('./json/film_list.json', 'r') as f:
             self.films_loaded = json.loads(f.read())
 
@@ -15,6 +18,10 @@ class MovieTierList(commands.Cog):
         for film in self.films_loaded:
             self.list += film['Name'] + "\n"
         await ctx.send(self.list)
+
+    @commands.command()
+    async def madd(self, ctx, arg):
+        pass
 
 def setup(client):
     client.add_cog(MovieTierList(client))
