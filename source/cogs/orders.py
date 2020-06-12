c050018ecd58243f4ccb6aa3c78dddd0cb437dc6
IHiFiveI <57842399+IHiFiveI@users.noreply.github.com>
2020-06-12 15:07:14 +0700

    double quotes replased by single ones

--- source/cogs/orders.py
+++ source/cogs/orders.py
@@ -6,30 +6,29 @@ class Orders(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.pongCount = 0
-        self.pongEnding = "!"
+        self.pongEnding = '!'
 
     @commands.command()
     async def slap(self, ctx):
-        # await ctx.send("ttetetetete test")
         print(ctx)
 
     @commands.command()
     async def ping(self, ctx):
         if self.pongCount == 4:
-            await ctx.send("you knew, im getting tired")
+            await ctx.send('you knew, im getting tired')
         elif self.pongCount == 6:
             self.pongEnding = ""
         elif self.pongCount == 13:
-            await ctx.send("can you stop, please?")
+            await ctx.send('can you stop, please?')
             self.pongEnding = "."
         elif self.pongCount == 16:
-            await ctx.send("ok bye retard i dont want to play with you anymore")
+            await ctx.send('ok bye retard i dont want to play with you anymore')
             await self.client.close()
         try:
-            await ctx.send("pong" + self.pongEnding)
+            await ctx.send('pong' + self.pongEnding)
             self.pongCount += 1
         except:
-            print("bot was ping-ponged out of existance\n")
+            print('bot was ping-ponged out of existance\n')
 
 def setup(client):
     client.add_cog(Orders(client))
