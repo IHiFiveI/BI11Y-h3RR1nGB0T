597e87f052074db811d0920ce88d50a6839a362e
IHiFiveI <57842399+IHiFiveI@users.noreply.github.com>
2020-06-14 15:20:49 +0700

    added 'remove movie' function; added some console and chat notifications

--- source/cogs/MovieTierList.py
+++ source/cogs/MovieTierList.py
@@ -32,5 +32,23 @@ class MovieTierList(commands.Cog):
         with open('./json/film_list.json', 'w', encoding="utf-8") as f:
             f.write(json.dumps(self.films_loaded, sort_keys = False, indent = 2))
 
+    @commands.command()
+    async def mrm(self, ctx, arg = ''):
+        self.is_deleted = False
+        for i in range(len(self.films_loaded)):
+            if self.films_loaded[i]["Name"] == arg:
+                self.films_loaded.pop(i)
+                self.is_deleted = True
+                break
+        with open('./json/film_list.json', 'w', encoding="utf-8") as f:
+            f.write(json.dumps(self.films_loaded, sort_keys=False, indent=2))
+        if self.is_deleted:
+            print('{} was successfully removed\n'.format(arg))
+            await ctx.send('{} был удалён из списка. И ладно.'.format(arg))
+        else:
+            print('{} was not found\n'.format(arg))
+            await ctx.send('{} не найден. Может, его никогда и не было?'.format(arg))
+
+
 def setup(client):
     client.add_cog(MovieTierList(client))
