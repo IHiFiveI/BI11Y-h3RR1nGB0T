10397261761b5a6c7cdff7cc7784cc3a7c2ab0be
IHiFiveI <57842399+IHiFiveI@users.noreply.github.com>
2020-06-22 20:54:47 +0700

    WolfQuotes made into discord.py class

--- source/cogs/WolfQuotes.py
+++ source/cogs/WolfQuotes.py
@@ -1,31 +1,46 @@
 import sys
 import random
 
+import discord
+from discord.ext import commands
 import json
 import cv2
 from PIL import Image, ImageDraw, ImageFont
 
-image = Image.open('./source/cogs/temp/file.png')
-draw = ImageDraw.Draw(image)
 
-with open('./json/wolf_quotes.json', 'r', encoding='UTF-8') as f:
-    qoutes = json.loads(f.read())
-text = random.choice(qoutes)
-font = ImageFont.truetype('./fonts/comicbd.ttf', size=72, encoding='UTF-8')
+class WolfQuotes(commands.Cog):
 
-text_width, text_height = draw.textsize(text, font=font)
-x_pos = image.size[0] * 0.5 - text_width * 0.5
-y_pos = 90 - text_height * 0.5
+    def __init__(self, client):
+        self.client = client
+        with open('./json/wolf_quotes.json', 'r', encoding='UTF-8') as f:
+            self.qoutes = json.loads(f.read())
+        self.image = Image.open('./images/file.png')
+        self.draw = ImageDraw.Draw(self.image)
+        self.font = ImageFont.truetype('./fonts/comicbd.ttf', size=72, encoding='UTF-8')
 
-i = 0
-while i < 6:
-    y_add = 5 if i < 3 else -5
-    x_add = 5 if not (i & 1) else -5
-    if i == 4 or not i:
-        x_add = 0
+    @commands.command()
+    async def wolf(self, ctx):
+        self.text = random.choice(self.qoutes)
 
-    draw.text((x_pos + x_add, y_pos + y_add), text, fill='rgb(0,0,0)', font=font, align="center")
-    i += 1
+        self.text_width, self.text_height = self.draw.textsize(self.text, font=self.font)
+        self.x_pos = self.image.size[0] * 0.5 - self.text_width * 0.5
+        self.y_pos = 90 - self.text_height * 0.5
 
-draw.text((x_pos, y_pos), text, fill='rgb(255,255,255)', font=font, align="center")
-image.save('./source/cogs/temp/file1.png', format=None)
+        i = 0
+        while i < 6:
+            self.y_add = 5 if i < 3 else -5
+            self.x_add = 5 if not (i & 1) else -5
+            if i == 4 or not i:
+                self.x_add = 0
+
+            self.draw.text((self.x_pos + self.x_add, self.y_pos + self.y_add), self.text,
+                           fill='rgb(0,0,0)', font=self.font, align="center")
+            i += 1
+
+        self.draw.text((self.x_pos, self.y_pos), self.text, fill='rgb(255,255,255)',
+                       font=self.font, align="center")
+        self.image.save('./images/file1.png', format=None)
+
+
+def setup(client):
+    client.add_cog(WolfQuotes(client))
