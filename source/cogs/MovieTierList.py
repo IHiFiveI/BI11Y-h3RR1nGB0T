27ba01510ea4e3cadd8603fe407a813ff61a9713
IHiFiveI <subject6gfrt@gmail.com>
2020-09-21 00:48:24 +0700

    Rewritted to support different servers

--- source/cogs/MovieTierList.py
+++ source/cogs/MovieTierList.py
@@ -1,5 +1,6 @@
 import json
 import discord
+import os
 from discord.ext import commands
 
 
@@ -7,13 +8,26 @@ class MovieTierList(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-        with open('./json/film_list.json', 'r') as f:
+        self.servers_own_films = ""
+        self.server_data_path = ""
+
+    def get_guild(self, arg = 0):
+        self.server_data_path = './json/{}'.format(arg)
+
+        if not os.path.exists(self.server_data_path):
+            os.mkdir(self.server_data_path)
+            with open(self.server_data_path + '/film_list.json', 'w') as fp:
+                fp.write('[\n]')
+        with open(self.server_data_path + '/film_list.json', 'r') as f:
             self.films_loaded = json.loads(f.read())
+
+        return self.films_loaded
 
     @commands.command()
     async def mlist(self, ctx):
+        self.servers_own_films = self.get_guild(ctx.message.guild.id)
         self.list = 'Список просмотренных фильмов:\n\n'
-        for film in self.films_loaded:
+        for film in self.servers_own_films:
             self.list += film['Name'] + "\n"
         if self.list != 'Список просмотренных фильмов:\n\n':
             await ctx.send(self.list)
@@ -22,6 +36,7 @@ class MovieTierList(commands.Cog):
 
     @commands.command()
     async def madd(self, ctx, *, arg=''):
+        self.servers_own_films = self.get_guild(ctx.message.guild.id)
         self.name = self.movie_rev_parse(arg, 1)
         self.new_info = [{
             "Name": self.name,
@@ -33,14 +48,15 @@ class MovieTierList(commands.Cog):
                 }
             ]
         }]
-        self.films_loaded += self.new_info
-        with open('./json/film_list.json', 'w', encoding="utf-8") as f:
-            f.write(json.dumps(self.films_loaded, sort_keys=False, indent=2))
+        self.servers_own_films += self.new_info
+        with open('./json/{}'.format(ctx.message.guild.id) + '/film_list.json', 'w', encoding="utf-8") as f:
+            f.write(json.dumps(self.servers_own_films, sort_keys=False, indent=2))
         print('{} was successfully added\n'.format(self.name))
         await ctx.send('{} добавлен в список'.format(self.name))
 
     @commands.command()
     async def maddrev(self, ctx, *, arg=''):
+        self.servers_own_films = self.get_guild(ctx.message.guild.id)
         self.review_name = self.movie_rev_parse(arg, 1)
         self.review_rate = self.movie_rev_parse(arg, 2)
         self.review_comment = self.movie_rev_parse(arg, 3)
@@ -49,35 +65,36 @@ class MovieTierList(commands.Cog):
             "reviewer_comment": self.review_comment,
             "rate": self.review_rate
         }]
-        for i in range(len(self.films_loaded)):
-            if self.films_loaded[i]["Name"] == self.review_name:
-                for j in range(len(self.films_loaded[i]["Commentary"])):
-                    if self.films_loaded[i]["Commentary"][j]["reviewer_id"] == (ctx.message.author.id or ""):
-                        self.films_loaded[i]["Commentary"][j][
+        for i in range(len(self.servers_own_films)):
+            if self.servers_own_films[i]["Name"] == self.review_name:
+                for j in range(len(self.servers_own_films[i]["Commentary"])):
+                    if self.servers_own_films[i]["Commentary"][j]["reviewer_id"] == (ctx.message.author.id or ""):
+                        self.servers_own_films[i]["Commentary"][j][
                             "reviewer_comment"] = self.review_comment
-                        self.films_loaded[i]["Commentary"][j]["rate"] = self.review_rate
+                        self.servers_own_films[i]["Commentary"][j]["rate"] = self.review_rate
                         break
-                    elif j == len(self.films_loaded[i]["Commentary"]) - 1:
-                        self.films_loaded[i]["Commentary"] += self.new_commentary
+                    elif j == len(self.servers_own_films[i]["Commentary"]) - 1:
+                        self.servers_own_films[i]["Commentary"] += self.new_commentary
                         break
-            elif i == len(self.films_loaded) - 1:
+            elif i == len(self.servers_own_films) - 1:
                 print('{} film was not found\n'.format(self.review_name))
                 await ctx.send('Фильм "{}" не был обнаружен. Добавь его с помошью ",madd"!'.format(self.review_name))
                 break
-        with open('./json/film_list.json', 'w', encoding="utf-8") as f:
-            f.write(json.dumps(self.films_loaded, sort_keys=False, indent=2))
+        with open('./json/{}'.format(ctx.message.guild.id) + '/film_list.json', 'w', encoding="utf-8") as f:
+            f.write(json.dumps(self.servers_own_films, sort_keys=False, indent=2))
 
     @commands.command()
     async def mrm(self, ctx, *, arg=''):
+        self.servers_own_films = self.get_guild(ctx.message.guild.id)
         self.is_deleted = False
         self.name = self.movie_rev_parse(arg, 1)
-        for i in range(len(self.films_loaded)):
-            if self.films_loaded[i]["Name"] == self.name:
-                self.films_loaded.pop(i)
+        for i in range(len(self.servers_own_films)):
+            if self.servers_own_films[i]["Name"] == self.name:
+                self.servers_own_films.pop(i)
                 self.is_deleted = True
                 break
-        with open('./json/film_list.json', 'w', encoding="utf-8") as f:
-            f.write(json.dumps(self.films_loaded, sort_keys=False, indent=2))
+        with open('./json/{}'.format(ctx.message.guild.id) + '/film_list.json', 'w', encoding="utf-8") as f:
+            f.write(json.dumps(self.servers_own_films, sort_keys=False, indent=2))
         if self.is_deleted:
             print('{} was successfully removed\n'.format(self.name))
             await ctx.send('{} был удалён из списка. И ладно.'.format(self.name))
@@ -87,19 +104,20 @@ class MovieTierList(commands.Cog):
 
     @commands.command()
     async def mstat(self, ctx, *, arg=''):
+        self.servers_own_films = self.get_guild(ctx.message.guild.id)
         self.review_name = self.movie_rev_parse(arg, 1)
         self.comment_to_show = ''
-        for i in range(len(self.films_loaded)):
-            if self.films_loaded[i]["Name"] == self.review_name:
-                for j in range(len(self.films_loaded[i]["Commentary"])):
+        for i in range(len(self.servers_own_films)):
+            if self.servers_own_films[i]["Name"] == self.review_name:
+                for j in range(len(self.servers_own_films[i]["Commentary"])):
                     try:
-                        self.comment_to_show += '```\nОбзорщик: ' + str(await self.client.fetch_user(self.films_loaded[i]["Commentary"][j]["reviewer_id"]))
+                        self.comment_to_show += '```\nОбзорщик: ' + str(await self.client.fetch_user(self.servers_own_films[i]["Commentary"][j]["reviewer_id"]))
                     except:
                         self.comment_to_show += '```\nОбзорщик: Неизвестный'
                     self.comment_to_show += '\nОценка: ' + \
-                        self.films_loaded[i]["Commentary"][j]["rate"]
+                        self.servers_own_films[i]["Commentary"][j]["rate"]
                     self.comment_to_show += '\\10\nКомментарий: ' + \
-                        self.films_loaded[i]["Commentary"][j]["reviewer_comment"]
+                        self.servers_own_films[i]["Commentary"][j]["reviewer_comment"]
                     self.comment_to_show += '\n```'
         if arg == '':
             print('blank line was entered as an arg\n')
